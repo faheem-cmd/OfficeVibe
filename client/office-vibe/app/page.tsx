a8f3297d6c9f07e25d7fb9d08726681e9cb1f4d4
Muhammed <faheem@metrictreelabs.com>
2023-12-06 23:46:37 +0530

    node module rest

--- client/office-vibe/app/page.tsx
+++ client/office-vibe/app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const rar = () => {
-  return <div>rar</div>;
+  return (
+    <div style={{ backgroundColor: "red", height: 200, width: "50%" }}>
+      raddr
+    </div>
+  );
 };
 
 export default rar;
